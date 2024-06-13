68ac7f22ac2f6a655bcfdcf5c4e2fa1d6232e1f3
amurph <aidanpmurphy@hotmail.com>
2024-06-12 21:59:19 -0700

    create source and include dirs

---
+++ source/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int getFiles(int argc, char *argv[]){
+	printf("running...\n");
+}
+
+int main(int argc, char *argv[]){
+	getFiles(argc, argv);
+	return 0;
+}

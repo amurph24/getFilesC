7c8d962410bfa5eecb96f843c6cb5abba11c75bd
amurph <aidanpmurphy@hotmail.com>
2024-06-13 19:32:04 -0700

    now reads csv file from command line

--- source/main.c
+++ source/main.c
@@ -1,8 +1,25 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int getFiles(int argc, char *argv[]){
-	printf("running...\n");
+#define ROW_CHAR_LIMIT 1024
+
+void printFileContents(FILE *file) {
+	char line[ROW_CHAR_LIMIT];
+	while(fgets(line,ROW_CHAR_LIMIT,file)) {
+		printf("%s", line);
+	}
+	return;
+}
+
+int getFiles(int argc, char *argv[]) {
+	printf("Printing contents of: %s\n", argv[1]);
+
+	FILE *file = fopen(argv[1], "r");
+	printFileContents(file);
+	fclose(file);
+
+	return 0;
 }
 
 int main(int argc, char *argv[]){

3df52d57868d4537a8ee510ed80e08dfe6968207
amurph <aidanpmurphy@hotmail.com>
2024-06-13 20:32:28 -0700

    implement findKeyIndex for searching csv's

--- source/main.c
+++ source/main.c
@@ -3,10 +3,35 @@
 #include <string.h>
 
 #define ROW_CHAR_LIMIT 1024
+#define KEY_COLUMN_NAME "key"
+#define CSV_DELIMITER ","
+
+int findKeyIndex(char *line, char *keyname){
+	char *token;
+	token = strtok(line, CSV_DELIMITER);
+	for(int i = 0; token; i++) {
+		// newline char disrupts check on last column header
+		token[strcspn( token, "\n")] = 0;
+		if (!strcmp(token, KEY_COLUMN_NAME))
+			return i;
+		token = strtok(NULL, CSV_DELIMITER);
+	}
+	return -1;
+}
 
 void printFileContents(FILE *file) {
 	char line[ROW_CHAR_LIMIT];
-	while(fgets(line,ROW_CHAR_LIMIT,file)) {
+	
+	// parse headings for index of key column
+	fgets(line, ROW_CHAR_LIMIT, file);
+	int key_column_index = findKeyIndex(line, KEY_COLUMN_NAME);
+	printf("index of 'key' column: %d\n", key_column_index);
+	if (key_column_index == -1) {
+		printf(".csv file does not contain a '%s' column, exiting...\n", KEY_COLUMN_NAME);
+		exit(1);
+	}
+	
+	while( fgets(line,ROW_CHAR_LIMIT,file) ) {
 		printf("%s", line);
 	}
 	return;

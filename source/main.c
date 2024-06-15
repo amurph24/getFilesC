266714358f87a3de73da47d70e2a0a32b40a1463
amurph <aidanpmurphy@hotmail.com>
2024-06-15 13:52:08 -0700

    comment out csv parsing for now

--- source/main.c
+++ source/main.c
@@ -6,16 +6,17 @@
 #include "read_data.h"
 
 int get_files(int argc, char *argv[]) {
-	printf("Printing contents of: %s\n", argv[1]);
-
+	/*
 	FILE *file = fopen(argv[1], "r");
 	if ( file != NULL) {
+		printf("Printing contents of: %s\n", argv[1]);
 		print_file_contents(file);
 		fclose(file);
 	}
 	else {
 		printf("%s not found\n", argv[1]);
 	}
+	*/
 
 	recursive_search(".", argv[1]);
 

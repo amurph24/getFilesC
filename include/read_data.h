28d83216223a79cd1ee05e7083a603b108dce814
amurph <aidanpmurphy@hotmail.com>
2024-06-15 11:29:07 -0700

    factor csv parsing into new header file

---
+++ include/read_data.h
@@ -0,0 +1,7 @@
+#ifndef READ_DATA
+#define READ_DATA
+
+
+void print_file_contents(FILE *file);
+
+#endif
--- source/main.c
+++ source/main.c
@@ -3,45 +3,7 @@
 #include <string.h>
 
 #include "recursive_search.h"
-
-#define ROW_CHAR_LIMIT 1024
-#define KEY_COLUMN_NAME "key"
-#define CSV_DELIMITER ","
-
-int find_key_index(char *line, char *key_name){
-	char *token;
-	token = strtok(line, CSV_DELIMITER);
-	for(int i = 0; token; i++) {
-		// newline char disrupts check on last column header
-		token[strcspn( token, "\n")] = 0;
-		if (!strcmp(token, key_name))
-			return i;
-		token = strtok(NULL, CSV_DELIMITER);
-	}
-	return -1;
-}
-
-void print_file_contents(FILE *file) {
-	char line[ROW_CHAR_LIMIT];
-	
-	// parse headings for index of key column
-	fgets(line, ROW_CHAR_LIMIT, file);
-	int key_column_index = find_key_index(line, KEY_COLUMN_NAME);
-	printf("index of 'key' column: %d\n", key_column_index);
-	if (key_column_index == -1) {
-		printf(
-			".csv file does not contain a '%s' column, exiting...\n",
-			KEY_COLUMN_NAME
-		);
-		exit(1);
-	}
-	
-	while( fgets(line,ROW_CHAR_LIMIT,file) ) {
-		printf("%s", line);
-	}
-	return;
-}
-
+#include "read_data.h"
 
 int get_files(int argc, char *argv[]) {
 	printf("Printing contents of: %s\n", argv[1]);

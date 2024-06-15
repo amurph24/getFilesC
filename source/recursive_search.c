8fc240fa16f88504770c7f91addc760ca7905100
amurph <aidanpmurphy@hotmail.com>
2024-06-14 21:46:14 -0700

    dynamically allocate memory for childPath

--- source/recursive_search.c
+++ source/recursive_search.c
@@ -19,18 +19,19 @@ int recursive_search_helper(char *path, char *key, int depth) {
 
 	// path is a file
 	if (!dir) {
-		printf("%s\n", path);	
+		printf("%s\n", path);
 		return 0;
 	}
 	
 	// path is a directory
-	if (depth >= MAX_DEPTH)
+	if (depth >= MAX_DEPTH) {
 		printf("'%s' exceeds max depth %d, cancelling search", path, MAX_DEPTH);
+		closedir(dir);
+		return 1;
+	}
 
-	char childPath[1024];
-	//char *selfDir = ".", *parentDir = "..";
-	//char slash = '/';
-
+	char *childPath = NULL;
+	int child_path_length;
 	struct dirent *dirChild;
 	
 	printf("%s\n", path);
@@ -38,6 +39,10 @@ int recursive_search_helper(char *path, char *key, int depth) {
 		
 		if ( !strcmp(dirChild->d_name, SELF_DIRECTORY) ) continue;
 		if ( !strcmp(dirChild->d_name, PARENT_DIRECTORY) ) continue;
+	
+		// resize childPath	
+		child_path_length = strlen(path) + 1 + strlen(dirChild->d_name) + 1;
+		childPath = (char*)malloc(child_path_length * sizeof(char));
 
 		// get new path for recursive call
 		strcpy(childPath, path);
@@ -46,6 +51,8 @@ int recursive_search_helper(char *path, char *key, int depth) {
 
 		//recursion
 		recursive_search_helper(childPath, key, depth + 1);
+
+		free(childPath);
 	}
 
 	closedir(dir);
